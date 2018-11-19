be0b58bc5f482da7c6f31cfe3e699875d40ae719
jonnypower <me@jonnypower.com>
2018-11-18 21:06:56 -0800

    Release 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule HLDSLogs.MixProject do
   def project do
     [
       app: :hlds_logs,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
